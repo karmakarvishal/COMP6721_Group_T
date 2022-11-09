e6e5a42e4e3683a585c05e346c625987c8fdf480
antoine <acyrntoine@gmail.com>
2022-11-08 22:32:53 -0500

    resnet 50 changes

--- Resnet50.py
+++ Resnet50.py
@@ -4,6 +4,7 @@
 #Add test dataset
 #organize everything into methods and divide it in different files
 #model taken from:  https://blog.paperspace.com/writing-resnet-from-scratch-in-pytorch/
+#architecture from: https://www.researchgate.net/figure/ResNet-18-Architecture_tbl1_322476121
 
 
 
@@ -28,6 +29,7 @@ preprocess = transforms.Compose([
     ),
 ])
 train_path = "Training"
+train_path = "/Users/antoinecyr/Documents/comp 6721/Project/Traffic-Sign-Classification/Training"
 
 #data set from https://btsd.ethz.ch/shareddata/
 train_data = torchvision.datasets.ImageFolder(root=train_path, transform=preprocess)
@@ -37,24 +39,26 @@ train_data_loader = torch.utils.data.DataLoader(train_data, batch_size=64, shuff
 print(train_data_loader)
 print(len(train_data_loader))
 
-image = plt.imread('Training\\00000\\01153_00000.ppm')
+'''image = plt.imread('Training\\00000\\01153_00000.ppm')
 fig, ax = plt.subplots()
 ax.imshow(image)
  
-plt.show()
+plt.show()'''
 
 class ResidualBlock(nn.Module):
     def __init__(self, in_channels, out_channels, stride = 1, downsample = None):
         super(ResidualBlock, self).__init__()
         self.conv1 = nn.Sequential(
                         nn.Conv2d(in_channels, out_channels, kernel_size = 3, stride = stride, padding = 1),
+                        
                         nn.BatchNorm2d(out_channels),
-                        nn.ReLU())
+                        nn.LeakyReLU())
         self.conv2 = nn.Sequential(
                         nn.Conv2d(out_channels, out_channels, kernel_size = 3, stride = 1, padding = 1),
-                        nn.BatchNorm2d(out_channels))
+                        nn.BatchNorm2d(out_channels)
+                        )
         self.downsample = downsample
-        self.relu = nn.ReLU()
+        self.relu = nn.LeakyReLU()
         self.out_channels = out_channels
         
     def forward(self, x):
@@ -68,25 +72,24 @@ class ResidualBlock(nn.Module):
         return out
     
 class ResNet(nn.Module):
-    def __init__(self, block, layers, num_classes = 62):
+    def __init__(self, block, num_classes = 62):
         super(ResNet, self).__init__()
         self.inplanes = 64
         self.conv1 = nn.Sequential(
-                        nn.Conv2d(3, 64, kernel_size = 7, stride = 2, padding = 3),
+                        nn.Conv2d(3, 64, kernel_size = 7, stride = 2, padding = 1),
                         nn.BatchNorm2d(64),
-                        nn.ReLU())
+                        nn.LeakyReLU())
         self.maxpool = nn.MaxPool2d(kernel_size = 3, stride = 2, padding = 1)
-        self.layer0 = self._make_layer(block, 64, layers[0], stride = 1)
-        self.layer1 = self._make_layer(block, 128, layers[1], stride = 2)
-        self.layer2 = self._make_layer(block, 256, layers[2], stride = 2)
-        self.layer3 = self._make_layer(block, 512, layers[3], stride = 2)
+        self.layer0 = self._make_layer(block, 64, 2, stride = 1)
+        self.layer1 = self._make_layer(block, 128, 2, stride = 2)
+        self.layer2 = self._make_layer(block, 256, 2, stride = 2)
+        self.layer3 = self._make_layer(block, 512, 2, stride = 2)
         self.avgpool = nn.AvgPool2d(7, stride=1)
         self.fc = nn.Linear(512, num_classes)
         
     def _make_layer(self, block, planes, blocks, stride=1):
         downsample = None
         if stride != 1 or self.inplanes != planes:
-            
             downsample = nn.Sequential(
                 nn.Conv2d(self.inplanes, planes, kernel_size=1, stride=stride),
                 nn.BatchNorm2d(planes),
@@ -121,7 +124,7 @@ learning_rate = 0.01
 use_cuda = torch.cuda.is_available()
 device = torch.device("cuda" if use_cuda else "cpu")
 
-model = ResNet(ResidualBlock, [3, 4, 6, 3]).to(device)
+model = ResNet(ResidualBlock,62).to(device)
 
 # Loss and optimizer
 criterion = nn.CrossEntropyLoss()
